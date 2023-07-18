968e6c246d70ab6c8bbe5d9cc52f6cb1ca87be2c
RCL <richardjameslopez@gmail.com>
2023-07-18 10:38:48 -0400

    Update layout.tsx
    small test

--- app/layouts/layout.tsx
+++ app/layouts/layout.tsx
@@ -4,7 +4,7 @@ import { TabGroup } from '#/ui/tab-group';
 import React from 'react';
 
 export const metadata = {
-  title: 'Nested Layouts',
+  title: 'Welcome',
 };
 
 export default async function Layout({
