03caddb01b701781097124f9fade1b5c59baede9
Rich CL <richcl@richs-air.lan>
2023-07-21 10:19:36 -0600

    Backend

--- lib/getBaseUrl.ts
+++ lib/getBaseUrl.ts
@@ -2,6 +2,6 @@ import { cache } from 'react';
 
 export const getBaseUrl = cache(() =>
   process.env.REACT_APP_API_URL
-    ? `${process.env.REACT_APP_API_URL}`
+    ? `${process.env.API_URL}`
     : `http://localhost:${process.env.PORT ?? 3000}`,
 );
