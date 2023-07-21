f1b9a57b209a310e61c8bbeb82d1af1af55ea5ec
Rich CL <richcl@richs-air.lan>
2023-07-21 10:00:37 -0600

    Backend

--- lib/getBaseUrl.ts
+++ lib/getBaseUrl.ts
@@ -1,7 +1,7 @@
 import { cache } from 'react';
 
 export const getBaseUrl = cache(() =>
-  process.env.VERCEL_URL
-    ? `https://app-dir.vercel.app`
+  process.env.REACT_APP_API_URL
+    ? `process.env.REACT_APP_API_URL`
     : `http://localhost:${process.env.PORT ?? 3000}`,
 );
