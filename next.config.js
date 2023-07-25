815682cd4c011adcab29756035f88b1116073278
Rich CL <richcl@Richs-MacBook-Air.local>
2023-07-25 15:53:11 -0600

    APIS

--- next.config.js
+++ next.config.js
@@ -1,13 +1,13 @@
 //@type {import('next').NextConfig}
-/*const nextConfig = {
+const nextConfig = {
   reactStrictMode: true, // Recommended for the `pages` directory, default in `app`.
   experimental: {
     appDir: true,
   },
 };
-*/
-//module.exports = nextConfig;
 
+module.exports = nextConfig;
+/*
 module.exports = {
   async headers() {
     return [
@@ -31,3 +31,4 @@ module.exports = {
     ];
   },
 };
+*/
