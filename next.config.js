b40491651c8c3ca1924a30a4ee3cd10a2c9dd3ab
Rich CL <richcl@Richs-MacBook-Air.local>
2023-07-25 11:08:44 -0600

    Backend

--- next.config.js
+++ next.config.js
@@ -13,7 +13,7 @@ module.exports = {
     return [
       {
         // matching all API routes
-        source: '/:path*',
+        source: '/api/(.*)',
         headers: [
           { key: 'Access-Control-Allow-Credentials', value: 'true' },
           { key: 'Access-Control-Allow-Origin', value: '*' },
