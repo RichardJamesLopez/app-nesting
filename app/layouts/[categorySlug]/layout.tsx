67a3e6b65ddc5762cb03d025e478f9397bbf56eb
Rich CL <richcl@Richs-MacBook-Air.local>
2023-07-27 09:18:50 -0600

    Component Splitting

--- app/layouts/[categorySlug]/layout.tsx
+++ app/layouts/[categorySlug]/layout.tsx
@@ -1,3 +1,47 @@
+// layout.tsx
+import { getCategories, getCategory } from '#/app/api/categories/getCategories';
+import { ClickCounter } from '#/ui/click-counter';
+import { TabGroup } from '#/ui/tab-group';
+import FormClientComponent from 'app/layouts/formClientComponent';
+
+export default async function Layout({
+  children,
+  params,
+}: {
+  children: React.ReactNode;
+  params: { categorySlug: string };
+}) {
+  const category = await getCategory({ slug: params.categorySlug });
+  const categories = await getCategories({ parent: params.categorySlug });
+
+  return (
+    <div className="space-y-9">
+      <div className="flex justify-between">
+        <TabGroup
+          path={`/layouts/${category.slug}`}
+          items={[
+            {
+              text: 'All',
+            },
+            ...categories.map((x) => ({
+              text: x.name,
+              slug: x.slug,
+            })),
+          ]}
+        />
+
+        <div className="self-start">
+          <ClickCounter />
+        </div>
+      </div>
+
+      <FormClientComponent />
+      <div>{children}</div>
+    </div>
+  );
+}
+
+/*
 //'use client';
 import { getCategories, getCategory } from '#/app/api/categories/getCategories';
 import { ClickCounter } from '#/ui/click-counter';
@@ -34,6 +78,9 @@ export default async function Layout({
       .catch(error => console.error('Error:', error));
   }, []); // Empty dependency array makes this run once on mount
   */
+
+/*
+  //adjust component architecture here
   return (
     <div className="space-y-9">
       <div className="flex justify-between">
@@ -60,3 +107,4 @@ export default async function Layout({
     </div>
   );
 }
+*/
--- app/route-groups/(main)/page.tsx
+++ app/route-groups/(main)/page.tsx
@@ -1,4 +1,4 @@
-import { ExternalLink } from '#/ui/external-link';
+//import { ExternalLink } from '#/ui/external-link';
 
 export default function Page() {
   return (
@@ -8,35 +8,3 @@ export default function Page() {
     </div>
   );
 }
-
-/*
-<h1 className="text-xl font-bold">Route Groups</h1>
-      <ul>
-        <li>
-          This example uses Route Groups to create layouts for different
-          sections of the app without affecting the URL structure.
-        </li>
-        <li>
-          Try navigating pages and noting the different layouts used for each
-          section.
-        </li>
-        <li>Route groups can be used to:</li>
-        <ul>
-          <li>Opt a route segment out of a shared layout.</li>
-          <li>Organize routes without affecting the URL structure.</li>
-          <li>
-            Create multiple root layouts by partitioning the top level of the
-            application.
-          </li>
-        </ul>
-      </ul>
-      
-      <div className="flex gap-2">
-        <ExternalLink href="https://nextjs.org/docs/app/building-your-application/routing/route-groups">
-          Docs
-        </ExternalLink>
-        <ExternalLink href="https://github.com/vercel/app-playground/tree/main/app/route-groups">
-          Code
-        </ExternalLink>
-      </div>
-      */
