80e8667c457d76e93f18142b698e86aa23505cf9
emrichmp <emrichmp@gmail.com>
2023-10-20 19:51:45 -0700

    Remove comments tab

--- ui/tab-group.tsx
+++ ui/tab-group.tsx
@@ -9,9 +9,11 @@ export type Item = {
 export const TabGroup = ({ path, items }: { path: string; items: Item[] }) => {
   return (
     <div className="flex flex-wrap items-center gap-2">
-      {items.map((item) => (
-        <Tab key={path + item.slug} item={item} path={path} />
-      ))}
+      {items
+        .filter((item) => item.slug !== 'comments')
+        .map((item) => (
+          <Tab key={path + item.slug} item={item} path={path} />
+        ))}
     </div>
   );
 };
