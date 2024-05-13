ad3d0b912b7d77fb59e5117030530c237261355b
Konstantin Kornev <konstantin.kornev.1@gmail.com>
2024-05-13 13:12:05 -0500

    feat: redirect to login page on unauthenticated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
 import { redirect } from "next/navigation";
+import { getServerAuthSession } from "~/server/auth";
 
-export default function HomePage() {
-  redirect("dashboard");
+export default async function HomePage() {
+  const session = await getServerAuthSession();
+
+  if (!session) redirect("api/auth/signin");
+  else redirect("dashboard");
 }
