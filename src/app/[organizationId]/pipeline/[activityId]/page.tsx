daa476cc30d33ec956fbfaa2a39806a730217191
Konstantin Kornev <konstantin.kornev.1@gmail.com>
2024-06-06 10:59:12 -0500

    refactor: abstract deal card into a separate component

--- src/app/[organizationId]/pipeline/[activityId]/page.tsx
+++ src/app/[organizationId]/pipeline/[activityId]/page.tsx
@@ -1,6 +1,3 @@
-import { format } from "date-fns";
-
-import { Card, CardContent, CardHeader, CardTitle } from "~/components/ui/card";
 import {
   Breadcrumb,
   BreadcrumbItem,
@@ -9,18 +6,10 @@ import {
   BreadcrumbPage,
   BreadcrumbSeparator,
 } from "~/components/ui/breadcrumb";
-import { Input } from "~/components/ui/input";
-import {
-  Select,
-  SelectTrigger,
-  SelectValue,
-  SelectContent,
-  SelectItem,
-} from "~/components/ui/select";
-import { Label } from "~/components/ui/label";
 import { api } from "~/trpc/server";
 
 import Comments from "./comments";
+import { DealCard } from "~/components/dealCard";
 
 export default async function ActivityPage({
   params,
@@ -44,57 +33,21 @@ export default async function ActivityPage({
   );
 
   const deal = await api.deal.get(params.activityId);
-  if (!deal) {
-    return (
-      <div className="mx-auto max-w-2xl">
-        {breadcrumb}
-        <p>Deal not found.</p>
-      </div>
-    );
-  }
 
   return (
     <div className="mx-auto max-w-2xl">
       {breadcrumb}
-      <Card className="mb-6">
-        <CardHeader>
-          <CardTitle>
-            {deal.visibility === "Show" ? deal.name : "---"}
-          </CardTitle>
-        </CardHeader>
-        <CardContent>
-          <form className="space-y-4">
-            <div>
-              <Label>Estimated Value</Label>
-              <Input
-                defaultValue={`$${new Intl.NumberFormat("en-US").format(deal.value)}`}
-                disabled
-              />
-            </div>
-            <div>
-              <Label>Pipeline Status</Label>
-              <Select disabled defaultValue={deal.status}>
-                <SelectTrigger>
-                  <SelectValue />
-                </SelectTrigger>
-                <SelectContent>
-                  <SelectItem value={deal.status}>{deal.status}</SelectItem>
-                </SelectContent>
-              </Select>
-            </div>
-            <div>
-              <Label>Last Edited</Label>
-              <div className="px-2 py-3 text-sm text-muted-foreground">
-                {format(new Date(deal.lastEdited), "MM/dd/yyyy, hh:mm aa")}
-              </div>
-            </div>
-          </form>
-        </CardContent>
-      </Card>
-      <Comments
-        dealId={params.activityId}
-        organizationId={params.organizationId}
-      />
+      {deal ? (
+        <>
+          <DealCard {...deal} />
+          <Comments
+            dealId={params.activityId}
+            organizationId={params.organizationId}
+          />
+        </>
+      ) : (
+        <p>Deal not found.</p>
+      )}
     </div>
   );
 }
